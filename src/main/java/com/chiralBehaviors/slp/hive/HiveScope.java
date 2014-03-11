5f169995782a1cc1288efe6699b095e2d43c335f
Hellblazer <hellblazer@me.com>
2014-03-11 10:40:20 -0700

    convert diagnostics to trace logging

--- src/main/java/com/chiralBehaviors/slp/hive/HiveScope.java
+++ src/main/java/com/chiralBehaviors/slp/hive/HiveScope.java
@@ -175,7 +175,9 @@ public class HiveScope implements ServiceScope {
         byte weight = state[0];
         byte priority = state[1];
         int len = state[2] << 8 | state[3] & 0xff;
-        log.info(String.format("Deserializing: %s", new String(state)));
+        if (log.isTraceEnabled()) {
+            log.trace(String.format("Deserializing: %s", new String(state)));
+        }
         String url = "service:" + new String(state, 4, len);
         Map<String, String> properties = propertiesFrom(state, len + 4);
         ServiceURL serviceUrl;
